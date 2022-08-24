0038308398ccf7183f73015e1904afe6e0c05a00
Sunny5787 <110806538+Sunny5787@users.noreply.github.com>
2022-08-24 12:57:21 +0530

    PRACTICAL-1 COMPLETED

---
+++ src/main/kotlin/Pr1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var a:Int = 1023166
+    var b:Double = 20.454
+    var c:Float = 1.12354456F
+    var d:Long = 10000000000000000
+    var e:Short = 3200
+    var f:Byte = 127
+    var g:Char = 'V'
+    var h:Boolean = true
+    var i:String = "SUNNY PATEL"
+    println("Integer value:$a")
+    println("Double value:$b")
+    println("Float value:$c")
+    println("Long value:$d")
+    println("Short value:$e")
+    println("Byte value:$f")
+    println("Char value:$g")
+    println("Boolean value:$h")
+    println("String value:$i")
+}
---
+++ src/main/kotlin/Pr1_2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var a:Int = 15
+    var b:Double = a.toDouble()
+    var c:String = "500"
+    var d:Int = c.toInt()
+    var e:Double = c.toDouble()
+    println("Integer to Double : $a to $b")
+    println("String to Integer : $c to $d")
+    println("String to Double : $c to $e")
+}
